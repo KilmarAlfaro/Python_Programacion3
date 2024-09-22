c250510e9160e88cf148187ae307ad4520a34883
KilmarAlfaro <157402339+KilmarAlfaro@users.noreply.github.com>
2024-09-22 11:19:00 -0600

    Add files via upload

---
+++ PruebaMySQL.py
@@ -0,0 +1,21 @@
+import MySQL
+import mysql.connector
+import sys
+
+db = mysql.connector.connect(
+    user="root",
+    password="1234",
+    host="localhost",
+    database="users"
+
+)
+cursor = db.cursor()
+
+sql = "insert into usuarios (Usuario, Contraseña) values (%s,%s)"
+
+values = (input("Ingrese el nombre de usuario: "),input("Ingrese la contraseña:  "))
+
+cursor.execute(sql, values)
+db.commit()
+
+
